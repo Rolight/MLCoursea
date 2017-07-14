a1a8bf2199cc6af6b91ed43928c4e8b7404bd390
rolight <flsnns@gmail.com>
2017-07-14 16:53:03 +0800

    finished multi varible problem

--- machine-learning-ex1/ex1/normalEqn.m
+++ machine-learning-ex1/ex1/normalEqn.m
@@ -1,6 +1,6 @@
 function [theta] = normalEqn(X, y)
-%NORMALEQN Computes the closed-form solution to linear regression 
-%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%NORMALEQN Computes the closed-form solution to linear regression
+%   NORMALEQN(X,y) computes the closed-form solution to linear
 %   regression using the normal equations.
 
 theta = zeros(size(X, 2), 1);
@@ -12,7 +12,8 @@ theta = zeros(size(X, 2), 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
+% X: (47, 3) y(47, 1)
+theta = pinv(X'*X)*X'*y
 
 
 % -------------------------------------------------------------
