e6fdd1b2489f82061dedfe263ac1593f46e86228
rolight <flsnns@gmail.com>
2017-07-26 11:48:20 +0800

    part 1. logistic regression finished

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,23 +7,22 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
+%               Compute t22gghe partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
-
-
-
+H = sigmoid(X * theta);
+J = log(H) .* (-y) - log(1 - H) .* (1 - y);
+J = sum(J) / m;
+grad = sum(X .* (H - y)) / m;
 
 
 
